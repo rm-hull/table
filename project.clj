fa8c2d0c614536d53efefc6d4f658fbb46bc55d1
Richard Hull <rm_hull@yahoo.co.uk>
2016-06-12 16:12:22 +0100

    Refresh project definition

--- project.clj
+++ project.clj
@@ -1,9 +1,25 @@
 (defproject rm-hull/table "0.6.0"
   :description "Display ascii tables for almost any data structure with ease"
   :url "http://github.com/rm-hull/table"
-  :license {:name "The MIT License"
-            :url "https://en.wikipedia.org/wiki/MIT_License"}
-  :repl-options  {:init-ns table.core}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles  {:1.4  {:dependencies  [[org.clojure/clojure "1.4.0"]]}}
-  :aliases {"all" ["with-profile" "dev:dev,1.4"]})
+  :license {
+    :name "The MIT License"
+    :url "http://opensource.org/licenses/MIT"}
+  :repl-options  {
+    :init-ns table.core}
+  :dependencies [
+    [org.clojure/clojure "1.8.0"]]
+  :scm {:url "git@github.com:rm-hull/infix.git"}
+  :plugins [
+    [lein-codox "0.9.5"] ]
+  :source-paths ["src"]
+  :jar-exclusions [#"(?:^|/).git"]
+  :codox {
+    :source-paths ["src"]
+    :output-path "doc/api"
+    :source-uri "http://github.com/rm-hull/infix/blob/master/{filepath}#L{line}"  }
+  :min-lein-version "2.6.1"
+  :profiles {
+    :dev {
+      :global-vars {*warn-on-reflection* true}
+      :plugins [
+        [lein-cloverage "1.0.6"]]}})
