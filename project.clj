67c5a7827394b4131d7103b5e9af5ed9c3a8dc70
Richard Hull <rm_hull@yahoo.co.uk>
2016-10-07 01:09:37 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -20,5 +20,7 @@
     :dev {
       :global-vars {*warn-on-reflection* true}
       :plugins [
-        [lein-codox "0.9.5"]
-        [lein-cloverage "1.0.6"]]}})
+        [lein-codox "0.10.0"]
+        [lein-cloverage "1.0.7"]]
+      :dependencies [
+        [org.clojure/clojure "1.8.0"]]}})
